0211f981c7f566899566ba9e812caacbcb94eb9d
Fariddmusa <faridam@code.edu.az>
2023-04-09 23:56:33 +0400

    Initial commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,12 @@
+﻿Student student1 = new Student();
+student1.Name = "Farid";
+student1.Surname = "Musayev";
+student1.Group = "P234";
+student1.Point = 100;
+student1.isGraduated = true;
+
+
+student1.GetFullName();
+student1.GetFullInfo();
+Console.WriteLine(student1.GetFullInfo());
+
---
+++ ConsoleApp6/Student.cs
@@ -0,0 +1,38 @@
+﻿internal class Student
+{
+    public string Name;
+    public string Surname;
+    public string Group;
+    public double Point;
+    public bool isGraduated;
+
+    public void  GetFullName()
+    {
+        Console.WriteLine(Name + " " + Surname);
+    }
+    public string GetFullInfo()
+    {
+        string info = $"{Name} {Surname},{Group},{Point}";
+
+        if (isGraduated)
+        {
+            info += ",Mezun olub";
+        }
+        else 
+        {
+            info+= ",Mezun olmayib";
+        }
+        if (Point > 80)
+        {
+            info += ",Gire biler";
+        }
+        else if  (Point <= 80)
+        {
+            info += ",Gire bilmez";
+        }
+        return info ;
+    }
+}
+   
+        
+   
